5e01d4222fbdd805a1b64dd80d58ef7b77bce081
Suuu <yisshiki39@gmail.com>
2026-05-22 00:23:39 +0900

    Use provided non-alcohol widget artwork

--- ios/NomoWidgets/NomoWidgets.swift
+++ ios/NomoWidgets/NomoWidgets.swift
@@ -180,7 +180,7 @@ private struct MediumStatusContent: View {
 
   private var hasEmbeddedStatusTitle: Bool {
     switch snapshot.statusKey {
-    case "can_drink_today", "want_drink", "light_drink", "want_drink_hard":
+    case "can_drink_today", "want_drink", "light_drink", "want_drink_hard", "non_alcohol":
       return true
     default:
       return false
