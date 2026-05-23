e2586d655a55d3a927fe08b89054e93eaf514379
Suuu <yisshiki39@gmail.com>
2026-05-24 00:41:56 +0900

    Lower feed post slightly

--- lib/features/home/presentation/home_feed_layout.dart
+++ lib/features/home/presentation/home_feed_layout.dart
@@ -1,7 +1,7 @@
 part of 'home_screen.dart';
 
 double _feedHeaderScrollInset(BuildContext context) {
-  return NomoPageHeader.contentTopInset(context) + 12;
+  return NomoPageHeader.contentTopInset(context) + 17;
 }
 
 const _feedBottomPageInset = 124.0;
