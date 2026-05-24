0b57d2066e21258ead47d11479b81b582175d71f
Suuu <yisshiki39@gmail.com>
2026-05-24 19:22:40 +0900

    fix: crop feed header bottom tighter

--- lib/features/home/presentation/home_feed_layout.dart
+++ lib/features/home/presentation/home_feed_layout.dart
@@ -1,7 +1,7 @@
 part of 'home_screen.dart';
 
 double _feedHeaderScrollInset(BuildContext context) {
-  return NomoPageHeader.contentTopInset(context) + 6;
+  return NomoPageHeader.contentTopInset(context) - 10;
 }
 
 const _feedBottomPageInset = 124.0;
