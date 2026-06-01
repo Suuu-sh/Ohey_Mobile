46f0e676f81ffff8e741448ee339a7356fce3b84
Suuu <yisshiki39@gmail.com>
2026-06-01 23:56:51 +0900

    Add ads to friends list

--- lib/features/friends/presentation/friends_screen.dart
+++ lib/features/friends/presentation/friends_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../core/application/ohey_user_controller.dart';
